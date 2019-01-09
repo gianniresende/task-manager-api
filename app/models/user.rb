6ab0506be6126734dd79f643e2484f92f0680d77
gianni <gianniresende@gmail.com>
2019-01-09 16:21:22 -0200

    Adding auth token and #info to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :auth_token
+
+  def info
+    #"#{email} - #{created_at}"
+    "#{email} - #{created_at}"
+  end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryBot.build(:user) }
+  #before { @user = FactoryBot.build(:user) }
+  let(:user) { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
+  #it { expect(@user).to respond_to(:email) }
   # pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('costa@nonato.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'returns email, created_at and a Token' do
+      user.save!
+      #allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
