a50c0d8282916e6a6089354ac01c84cbc0a6df80
gianni <gianniresende@gmail.com>
2019-01-09 16:40:45 -0200

    Updating User#info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,9 +18,11 @@ RSpec.describe User, type: :model do
   describe '#info' do
     it 'returns email, created_at and a Token' do
       user.save!
+
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
       #allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
-      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
     end
   end
 end
