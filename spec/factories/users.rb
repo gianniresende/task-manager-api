f6b583138bd2410b3b71a233dfbb1a380b8fe5c6
gianni <gianniresende@gmail.com>
2019-01-09 11:05:20 -0200

    Adding create action to the users controller whit tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,12 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    username Faker::Name.first_name.downcase
-    password 'Sample:1'
-    password_confirmation 'Sample:1'
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    active true
-    confirmed true
+    #username Faker::Name.first_name.downcase
+    password '123456'
+    password_confirmation '123456'
+    #first_name Faker::Name.first_name
+    #last_name Faker::Name.last_name
+    #active true
+    #confirmed true
   end
 end
