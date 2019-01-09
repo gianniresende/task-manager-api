26f9eefe4d2ca81a8f2483e1dd2f4233009af62f
gianni <gianniresende@gmail.com>
2019-01-09 20:06:13 -0200

    Refactoring the authorization and adding user_signed_in? method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 module Authenticable
+  
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def authenticate_with_token!
+    render json: { errors: 'Unauthorized access!' }, status: 401 unless current_user.present?
   end
 end
