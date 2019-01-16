d688d3038817234eeed8e9d28525de14d12f215b
gianni <gianniresende@gmail.com>
2019-01-16 12:24:04 -0200

    Last update

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -2,7 +2,7 @@
 
 class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :done, :deadline, :created_at, :updated_at,
-             :short_description, :is_late, :user_id
+             :short_description, :is_late, :user_id, :deadline_to_br
 
   def short_description
     object.description[0..40] if object.description.present?
@@ -10,6 +10,10 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
 
   def is_late
     Time.current > object.deadline if object.deadline.present?
+  end
+
+  def deadline_to_br
+    I18n.l(object.deadline, formats: :datetime) if object.deadline.present?
   end
 
   belongs_to :user
